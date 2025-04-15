3cc2008df2490d9b7f842e49b27c86cdc77e264b
nasrullohfajar <nasrullohfajar10@gmail.com>
2025-04-15 11:11:50 +0700

    feat: add table result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Result from "./components/Result";
 function App() {
   return (
     <>
       <Header />
       <UserInput/>
+      <Result/>
     </>
   );
 }

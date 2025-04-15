bd4559e52cfc491390e3866049da6186904d8679
nasrullohfajar <nasrullohfajar10@gmail.com>
2025-04-15 10:16:55 +0700

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div id="header">
+      <img src={'public/investment-calculator-logo.png'} alt="" />
+      <h1>Invesment Calculator</h1>
+    </div>
+  );
+};
+
+export default Header;
